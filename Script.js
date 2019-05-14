174b0c01c6beddb11c6b43a0aa21e9d1976560e3
Nikita <48110075+Nikidos@users.noreply.github.com>
2019-05-14 10:14:43 +0300

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,6 @@
+Script.js
+function clean(obj) {
+    document.getElementById("field1").value = '';
+    document.getElementById("field2").value = '';
+    document.getElementById("textArea").value = '';
+}
